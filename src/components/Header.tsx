a3a1067d939894722c7a48e6faad8eec847be3cc
Denis <ankalogan@gmail.com>
2021-12-10 02:14:10 +0300

    social netwowk first steps

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Header = function () {
+    return (
+        <header className="header">
+            <img src="http://logok.org/wp-content/uploads/2014/05/Total-logo-earth-880x660.png" alt="seas"/>
+        </header>
+    )
+}
---
+++ src/components/Profile.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+export const Profile = function () {
+    return (
+        <div className="content">
+            <div>
+                <img src="https://oceanservice.noaa.gov/facts/sea.jpg" alt="seas"/>
+            </div>
+            <div>
+                ava + desc
+            </div>
+            <div>
+                my posts
+                <div>
+                    new post
+                </div>
+                <div>
+                    post 1
+                </div>
+                <div>
+                    post 2
+                </div>
+            </div>
+        </div>
+    )
+}
