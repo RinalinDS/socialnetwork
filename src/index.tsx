acb30c8d5818d0eaf915438eeb49440764ecd089
RinalisDS <denispilyutin@gmail.com>
2022-02-14 21:17:37 +0200

    fixed mistakes with index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -17,6 +17,12 @@ export const rerenderEntireTree = () => {
 
 }
 
+rerenderEntireTree()
+
+store.subscribe(()=> {
+    rerenderEntireTree()
+})
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
