9b0f77d34f338ad8973ec7847137e776d6784382
RinalisDS <denispilyutin@gmail.com>
2022-05-16 16:16:38 +0300

    refactor , refactor , refactor

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -8,38 +8,33 @@ import {AppRootStateType} from "../../redux/store";
 import {FriendsType} from '../../Types/AppTypes';
 
 
-
 export const Navbar = function () {
 
-    const friendsState = useSelector<AppRootStateType, Array<FriendsType>>(state => state.friends)
+  const friendsState = useSelector<AppRootStateType, Array<FriendsType>>(state => state.friends)
 
-    return (
-        <nav className={s.nav}>
-            <div>
-                <NavLink to={"/profile/"} activeClassName={s.active_link}>Profile</NavLink>
-
-            </div>
-            <div>
-                <NavLink to={"/dialogs/"} activeClassName={s.active_link}>Messages</NavLink>
-            </div>
-            <div>
-                <NavLink to={'/users/'} activeClassName={s.active_link}>Users</NavLink>
-            </div>
-            <div>
-                <NavLink to={'/news/'} activeClassName={s.active_link}>News</NavLink>
-            </div>
-            <div>
-                <NavLink to={'/music/'} activeClassName={s.active_link}> Music</NavLink>
-            </div>
-            <div>
-                <NavLink to={'/settings/'} activeClassName={s.active_link}>Settings</NavLink>
-            </div>
-
-            <div>
-
-                <Friends friends={friendsState}/>
-
-            </div>
-        </nav>
-    )
+  return (
+    <nav className={s.nav}>
+      <div>
+        <NavLink to={"/profile/"} activeClassName={s.active_link}>Profile</NavLink>
+      </div>
+      <div>
+        <NavLink to={"/dialogs/"} activeClassName={s.active_link}>Messages</NavLink>
+      </div>
+      <div>
+        <NavLink to={'/users/'} activeClassName={s.active_link}>Users</NavLink>
+      </div>
+      <div>
+        <NavLink to={'/news/'} activeClassName={s.active_link}>News</NavLink>
+      </div>
+      <div>
+        <NavLink to={'/music/'} activeClassName={s.active_link}> Music</NavLink>
+      </div>
+      <div>
+        <NavLink to={'/settings/'} activeClassName={s.active_link}>Settings</NavLink>
+      </div>
+      <div>
+        <Friends friends={friendsState}/>
+      </div>
+    </nav>
+  )
 }
